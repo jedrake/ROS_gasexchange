af7c0aa3fb35aa115152811c398022ebbf4dcb9e
jedrake <je.drake@uws.edu.au>
2016-09-14 16:26:02 +1000

    updated Tuzet model fitting. particularly changed the parameters given the changes in Martin's equation.

--- R/fitTuzets.R
+++ R/fitTuzets.R
@@ -48,9 +48,15 @@ tuzets.cost <- function(pars,dat,fit=1,g1=5,Vcmax=80,adjVcmax=0){
   K <- pars[3]
   
   
-  #- adjust Vcmax according to a beta function. This uses the pira parameters
+  #- adjust Vcmax according to a beta function. 
   if (adjVcmax==1){
-    Vcmax_a <- Vcmax*((dat$TDR/100 - 0.01)/(0.17-0.01))^0.34
+    df <- data.frame(species=c("cacu","eusi","eute","pira"),
+                     Xl = c(.01,.01,.01,.01),
+                     Xh = c(.19,.2,.18,.12),
+                     q = c(.55,.38,.34,.48))
+    whichline <- which(df$species==dat$Species[1])
+    
+    Vcmax_a <- Vcmax*((dat$TDR/100 - df[whichline,"Xl"])/(df[whichline,"Xh"]-df[whichline,"Xl"]))^df[whichline,"q"]
     Vcmax_a[which(Vcmax_a>Vcmax)] <- Vcmax
   }
   
@@ -91,37 +97,122 @@ tuzets.cost <- function(pars,dat,fit=1,g1=5,Vcmax=80,adjVcmax=0){
 
 #------------------------------------------------------------------------------------------------------------------
 #- set up model estimate for a selected species
-dat <- subset(dat.all,Species=="eusi")
-dat <- dat[complete.cases(dat),]
+dat.all <- dat.all[complete.cases(dat.all),]
+dat.list <- split(dat.all,dat.all$Species)
 
 #- define lower and upper limits of parameter estimates
 # pars are psiv, SF,  and K
 lower <- c(-10,0.5,0)
 upper <- c(0,25,20)
-NPmax <- 40
+NPmax <- 100
 maxiter <- 30
 
-#- fit the model, extract the best parameters, and rerun the model to get the predicted values  
-DEfit <- DEoptim(fn=tuzets.cost,lower=lower,upper=upper,dat=dat,g1=5,Vcmax=80,adjVcmax=1,
-                     fit=1,DEoptim.control(NP = NPmax,itermax=maxiter)) #perhaps adjust reltol, NP=20, itermax = 100
-DEfit.best <- unname(DEfit$optim$bestmem)
-DEpred <- tuzets.cost(pars=DEfit.best,dat=dat,fit=0,g1=5,Vcmax=80,adjVcmax=1)
 
+#- set seed for repeatability
+set.seed(1234)
 
-#- overlay plot of estimated values and model
-windows();par(mfrow=c(2,2))
-
-#- photo vs. cond
-plot(Photo~Cond,data=dat)
-points(ALEAF~GS,data=DEpred,col="red")
-legend("bottomright",pch=1,col=c("black","red"),legend=c("Obs","Pred"))
-
-plot(dat$Photo~DEpred$ALEAF,xlab="Pred Photo",ylab="Photo");abline(0,1)
-plot(dat$Cond~DEpred$GS,xlab="Pred cond",ylab="Cond");abline(0,1)
-plot(dat$LWP.md~DEpred$PSIL,xlab="Pred Psi leaf",ylab="Psi leaf");abline(0,1)
-title(main=dat$Species[1],outer=T,line=-2)
-
-windows();par(mfrow=c(1,1))
-plot(Cond~LWP.md,data=dat)
-points(DEpred$GS~dat$LWP.md,col="red")
 #------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+#- model with no change in photosynthetic capacity
+DEfit <- DEfit.best <- DEpred <- list()
+for (i in 1:length(dat.list)){
+  tofit <- dat.list[[i]]
+  
+  #- fit the model, extract the best parameters, and rerun the model to get the predicted values  
+  DEfit[[i]] <- DEoptim(fn=tuzets.cost,lower=lower,upper=upper,dat=tofit,g1=5,Vcmax=80,adjVcmax=0,
+                   fit=1,DEoptim.control(NP = NPmax,itermax=maxiter)) #perhaps adjust reltol, NP=20, itermax = 100
+  DEfit.best[[i]] <- unname(DEfit[[i]]$optim$bestmem)
+  DEpred[[i]] <- tuzets.cost(pars=DEfit.best[[i]],dat=tofit,fit=0,g1=5,Vcmax=80,adjVcmax=0)
+  DEpred[[i]]$Species <- tofit$Species[1]
+  DEpred[[i]]$Date <- tofit$Date
+  
+}
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+#- this time, model with a change in Vcmax with decreasing TDR
+
+DEfit.nsl <- DEfit.best.nsl <- DEpred.nsl <- list()
+for (i in 1:length(dat.list)){
+  tofit <- dat.list[[i]]
+  
+  #- fit the model, extract the best parameters, and rerun the model to get the predicted values  
+  DEfit.nsl[[i]] <- DEoptim(fn=tuzets.cost,lower=lower,upper=upper,dat=tofit,g1=5,Vcmax=80,adjVcmax=1,
+                        fit=1,DEoptim.control(NP = NPmax,itermax=maxiter)) #perhaps adjust reltol, NP=20, itermax = 100
+  DEfit.best.nsl[[i]] <- unname(DEfit.nsl[[i]]$optim$bestmem)
+  DEpred.nsl[[i]] <- tuzets.cost(pars=DEfit.best.nsl[[i]],dat=tofit,fit=0,g1=5,Vcmax=80,adjVcmax=1)
+  DEpred.nsl[[i]]$Species <- tofit$Species[1]
+  DEpred.nsl[[i]]$Date <- tofit$Date
+  
+}
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+#- forward prediction of model responses to leaf water potential 
+
+#- params with and without Vcmax change
+do.call(rbind,DEfit.best)
+#[1,] -0.41602447 2.1745043 6.443134
+#[2,] -0.01191479 1.2121494 3.147091
+#[3,] -0.07180947 0.9570507 3.426615
+#[4,] -0.72772242 2.7658779 4.099408
+do.call(rbind,DEfit.best.nsl)
+#[1,] -2.671953 24.875104 7.132115
+#[2,] -2.574391  1.465136 5.349148
+#[3,] -3.033131 24.788989 6.328085
+#[4,] -1.282950  2.130503 4.625018
+
+psileaf <- seq(from=-8,to=0,length.out=101)
+pred.psileaf <- list()
+for(i in 1:length(DEfit.best)){
+  
+  #- pull out the parameters from the pars vector
+  psiv <- DEfit.best[[i]][1]
+  SF <- DEfit.best[[i]][2]
+  K <- DEfit.best[[i]][3]
+  
+  pred.psileaf[[i]] <- photosyn(SF=SF, PSIV=psiv, G0=0.0005, VCMAX=80,G1=8,K=K,
+                  CS=400,WEIGHTEDSWP=psileaf, VPD=1.2,PAR=1800,TLEAF=25)
+  pred.psileaf[[i]]$Species <- levels(dat.all$Species)[i]
+}
+preddat <- do.call(rbind,pred.psileaf)
+
+
+
+#- overlay plots of data for gs vs. psileaf relationship along with model output
+dat.m <- summaryBy(Cond+Photo+LWP+LWP.md+TDR~Species+Treat+Date,FUN=c(mean,standard.error),data=dat.all)
+
+
+windows(25,14)
+par(mfrow=c(1,2),oma=c(1,1,0,0),mar=c(6.25,6.25,0.25,0.25))
+colors <- brewer.pal(5,"Accent")[c(1,2,3,5)]#brewer.pal(4,"Set1")
+
+#- plot Conductance
+plotBy(GS~PSILIN|Species,data=preddat,type="l",legend=F,lwd=3,ylim=c(0,0.6),xlim=c(-8,0),
+       col = colors,axes=F,xlab="",ylab="")
+plotBy(Cond.mean~LWP.md.mean|Species,dat=dat.m,col=colors,pch=16,add=T,legend=F,cex=1.5)
+legend("top",legend=c("Cacu","Eusi","Eute","Pira"),pch=16,col=colors,ncol=2,cex=1.2)
+magaxis(side=c(1,2,4),labels=c(1,1,0),frame.plot=T,las=1,cex.axis=1.2)
+title(xlab=expression(Psi[l]~(MPa)),ylab=expression(g[s]~(mol~m^-2~s^-1)),cex.lab=1.5)
+legend("topright",letters[1],bty="n",cex=1.1,inset=0.02)
+#- plot Photo
+plotBy(ALEAF~PSILIN|Species,data=preddat,type="l",legend=F,lwd=3,ylim=c(0,25),xlim=c(-8,0),
+       col = colors,axes=F,xlab="",ylab="")
+plotBy(Photo.mean~LWP.md.mean|Species,dat=dat.m,col=colors,pch=16,add=T,legend=F,cex=1.5)
+#legend("top",legend=c("Cacu","Eusi","Eute","Pira"),fill=colors,ncol=2,cex=1.2)
+magaxis(side=c(1,2,4),labels=c(1,1,0),frame.plot=T,las=1,cex.axis=1.2)
+title(xlab=expression(Psi[l]~(MPa)),ylab=expression(A[sat]~(mu*mol~m^-2~s^-1)),cex.lab=1.5)
+legend("topright",letters[2],bty="n",cex=1.1,inset=0.02)
+
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+
