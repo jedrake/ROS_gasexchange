7faec6f188c76b43cd31e1c595a693176c644e48
jedrake <je.drake@uws.edu.au>
2016-11-16 14:46:44 +1100

    replaced the beta models associated with leaf water potential with exponential models, as per Belinda's suggestion.

--- R/fitBeta_g1_nsl_LWP.R
+++ R/fitBeta_g1_nsl_LWP.R
@@ -24,6 +24,9 @@ NSL <- returnVcmaxa()
 NSLpars <- summaryBy(NSL+Photo+Cond+Ci+TDR~Species+gxDate+Treat,data=NSL,FUN=mean,keep.names=T,na.rm=T)
 NSLpars2 <- merge(NSLpars,lwp.m,by.x=c("Species","Treat","gxDate"),by.y=c("Species","Treat","gxDate"))
 #NSLpars2 <- subset(NSLpars2,gxDate != as.Date("2012-10-31"))
+#- remove a problematic date for Pira
+NSLpars2 <- NSLpars2[-which(NSLpars2$gxDate==as.Date("2013-04-04") & NSLpars2$Species=="pira"),]
+
 
 #---------------------------------------------------------------------------------------------------------- 
 #---------------------------------------------------------------------------------------------------------- 
@@ -36,13 +39,14 @@ startlist = list(Xlow = 1, Xhigh=7, q = 4)
 dat.l <- split(dat,dat$Species)
 
 #- preallocate empty lists to catch output
-fit.sp <- newdat <-  list()
+fit.sp <- fit.sp.exp <- newdat <-  list()
 
 #- loop over each element of the list, fit data
 for (i in 1:length(dat.l)){
   dat.temp <- dat.l[[i]]
   dat.temp$Species <- factor(dat.temp$Species)
   dat.temp$LWPpos <- dat.temp$LWP.pd+11
+  dat.temp$LWP <- dat.temp$LWP.pd
   
   #- fit the model
   if (type=="g1") dat.temp$Yval <- dat.temp$g1/max(dat.temp$g1)
@@ -50,18 +54,28 @@ for (i in 1:length(dat.l)){
   fit.sp[[i]] <- nls(Yval ~ ((LWPpos-Xlow)/(Xhigh-Xlow))^q,start=startlist,data=dat.temp,algorithm="port",
                      lower=c(1,6,1.5),upper=c(5,11,10))
   
+  #-----
+  #- try fitting an exponential model instead
+  fit.sp.exp[[i]] <- nls(Yval ~ I(a * exp(b * (LWP))), data = dat.temp, start = list(a = 1, b = 0), trace = F)
+  #plot(Yval~LWP.pd,data=dat.temp)
+  #s = seq(from=-8,to=0,length.out=101)
+  #lines(s, predict(fit.sp.exp[[i]], list(LWP.pd = s)), col = "red")
+  #-----
+  
   
   # get predicted values and 95% confidence intervals by bootstrapping
-  if (type=="g1") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWPpos=seq(from=coef(fit.sp[[i]])["Xlow"]+0.01,to=max(dat.temp$LWPpos),length.out=99),lower=NA,upper=NA)
-  if (type=="NSL") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWPpos=seq(from=coef(fit.sp[[i]])["Xlow"]+0.01,to=coef(fit.sp[[i]])["Xhigh"],length.out=99),lower=NA,upper=NA)
-  newdat[[i]]$wpred <- predict(fit.sp[[i]],newdat[[i]],level=0,se.fit=T)
+  if (type=="g1") newdat[[i]] <- newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWP=seq(from=-10,to=0,length.out=101),lower=NA,upper=NA)
+  if (type=="NSL") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWP.pd(from=-10,to=0,length.out=101),lower=NA,upper=NA)
+  #newdat[[i]]$wpred <- predict(fit.sp[[i]],newdat[[i]],level=0,se.fit=T)
+  newdat[[i]]$wpred <- predict(fit.sp.exp[[i]],newdat[[i]],level=0,se.fit=T)
   
   
   rm(b)
-  b <- bootCase(fit.sp[[i]],B=300)
+  b <- bootCase(fit.sp.exp[[i]],B=300)
   for(j in 1:nrow(newdat[[i]])){
     rm(b02)
-    b02 <- ((newdat[[i]]$LWPpos[j]-b[,"Xlow"])/(b[,"Xhigh"]-b[,"Xlow"]))^b[,"q"]
+    #b02 <- ((newdat[[i]]$LWPpos[j]-b[,"Xlow"])/(b[,"Xhigh"]-b[,"Xlow"]))^b[,"q"]
+    b02 <- b[,"a"]*exp((newdat[[i]]$LWP[j])*b[,"b"])
     
     newdat[[i]]$lower[j] <- unname(quantile(b02,na.rm=T,probs=c(0.025,0.975)))[1]
     newdat[[i]]$upper[j] <- unname(quantile(b02,na.rm=T,probs=c(0.025,0.975)))[2]
@@ -70,9 +84,11 @@ for (i in 1:length(dat.l)){
   
 }
 newdatg1 <- newdat
-fit.spg1.LWP <- fit.sp
+#fit.spg1.LWP <- fit.sp
+fit.spg1.LWP <- fit.sp.exp
 rm(newdat)
 rm(fit.sp)
+rm(fit.sp.exp)
 g1_TDR_beta2 <- list(fit.spg1.LWP,newdatg1)
 #---------------------------------------------------------------------------------------------------------- 
 #---------------------------------------------------------------------------------------------------------- 
@@ -91,13 +107,14 @@ startlist = list(Xlow = 1, Xhigh=7, q = 3)
 dat.l <- split(dat,dat$Species)
 
 #- preallocate empty lists to catch output
-fit.sp <- newdat <-  list()
+fit.sp <- fit.sp.exp <-  newdat <-  list()
 
 #- loop over each element of the list, fit data
 for (i in 1:length(dat.l)){
   dat.temp <- dat.l[[i]]
   dat.temp$Species <- factor(dat.temp$Species)
   dat.temp$LWPpos <- dat.temp$LWP.pd+11
+  dat.temp$LWP <- dat.temp$LWP.pd
   
   #- fit the model
   if (type=="g1") dat.temp$Yval <- dat.temp$g1/max(dat.temp$g1)
@@ -108,21 +125,31 @@ for (i in 1:length(dat.l)){
                      lower=c(1,7,1.1),upper=c(2,11,6))
   if(i==4) startlist = list(Xlow = 8.5, Xhigh=11, q = 2)
   
-  if(i ==4)fit.sp[[i]] <- nls(Yval ~ ((LWPpos-Xlow)/(Xhigh-Xlow))^q,start=startlist,data=dat.temp,algorithm="port",trace=F,
-                              lower=c(8.5,10,1.1),upper=c(9.5,11,6))
+  #if(i ==4)fit.sp[[i]] <- nls(Yval ~ ((LWPpos-Xlow)/(Xhigh-Xlow))^q,start=startlist,data=dat.temp,algorithm="port",trace=F,
+  #                            lower=c(8.5,10,1.1),upper=c(9.5,11,6))
   
+  
+  #-----
+  #- try fitting an exponential model instead
+  fit.sp.exp[[i]] <- nls(Yval ~ I(a * exp(b * (LWP))), data = dat.temp, start = list(a = 1, b = 0), trace = F)
+  #plot(Yval~LWP.pd,data=dat.temp)
+  #s = seq(from=-8,to=0,length.out=101)
+  #ines(s, predict(fit.sp.exp[[i]], list(LWP = s)), col = "red")
+  #-----
   
   # get predicted values and 95% confidence intervals by bootstrapping
   if (type=="g1") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWPpos=seq(from=coef(fit.sp[[i]])["Xlow"]+0.01,to=max(dat.temp$LWPpos),length.out=99),lower=NA,upper=NA)
-  if (type=="NSL") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWPpos=seq(from=coef(fit.sp[[i]])["Xlow"],to=coef(fit.sp[[i]])["Xhigh"],length.out=99),lower=NA,upper=NA)
-  newdat[[i]]$wpred <- predict(fit.sp[[i]],newdat[[i]],level=0,se.fit=T)
-  
+  #if (type=="NSL") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWPpos=seq(from=coef(fit.sp[[i]])["Xlow"],to=coef(fit.sp[[i]])["Xhigh"],length.out=99),lower=NA,upper=NA)
+  #newdat[[i]]$wpred <- predict(fit.sp[[i]],newdat[[i]],level=0,se.fit=T)
+  if (type=="NSL") newdat[[i]] <- expand.grid(Species=levels(dat.temp$Species), LWP=seq(from=-10,to=0,length.out=101),lower=NA,upper=NA)
+  newdat[[i]]$wpred <- predict(fit.sp.exp[[i]],newdat[[i]],level=0,se.fit=T)
   
   rm(b)
-  b <- bootCase(fit.sp[[i]],B=300)
+  b <- bootCase(fit.sp.exp[[i]],B=300)
   for(j in 1:nrow(newdat[[i]])){
     rm(b02)
-    b02 <- ((newdat[[i]]$LWPpos[j]-b[,"Xlow"])/(b[,"Xhigh"]-b[,"Xlow"]))^b[,"q"]
+    #b02 <- ((newdat[[i]]$LWPpos[j]-b[,"Xlow"])/(b[,"Xhigh"]-b[,"Xlow"]))^b[,"q"]
+    b02 <- b[,"a"]*exp((newdat[[i]]$LWP[j])*b[,"b"])
     
     newdat[[i]]$lower[j] <- unname(quantile(b02,na.rm=T,probs=c(0.025,0.975)))[1]
     newdat[[i]]$upper[j] <- unname(quantile(b02,na.rm=T,probs=c(0.025,0.975)))[2]
@@ -133,7 +160,7 @@ for (i in 1:length(dat.l)){
 
 newdatNSL <- newdat
 rm(newdat)
-fit.spNSL.LWP <- fit.sp
+fit.spNSL.LWP <- fit.sp.exp
 rm(fit.sp)
 NSL_TDR_beta2 <- list(fit.spNSL.LWP,newdatNSL)
 #---------------------------------------------------------------------------------------------------------- 
